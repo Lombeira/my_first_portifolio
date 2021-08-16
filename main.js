17e5eaef32f93a42f3848aee12fe15902873cb01
Lombeira <glomenha@gmail.com>
2021-08-16 16:15:55 -0300

    Fully implemented

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 const nav = document.querySelector('#header nav');
 const toggle = document.querySelectorAll('nav .toggle');
-
 for (const element of toggle) {
   element.addEventListener('click', () => {
     nav.classList.toggle('show');
@@ -11,22 +10,30 @@ for (const element of toggle) {
 
 // quando clicar em um item do menubar, esconder o menu
 const links = document.querySelectorAll('nav ul li a');
-
 for (const link of links) {
   link.addEventListener('click', () => {
     nav.classList.remove('show');
   });
 }
 
-// mudar o header da página quando der scroll
+// Mudar o header da página quando der scroll
+
 const header = document.querySelector('#header');
 const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', () => {
+function changeHeaderWhenScroll() {
   window.scrollY >= navHeight
     ? header.classList.add('scroll')
     : header.classList.remove('scroll');
-});
+}
+
+const backToTopButton = document.querySelector('.back-to-top');
+// Botão voltar para o topo
+function backToTop() {
+  window.scrollY >= 1300
+    ? backToTopButton.classList.add('show')
+    : backToTopButton.classList.remove('show');
+}
 
 // Testimonials carousel with slider
 
@@ -37,6 +44,12 @@ const swiper = new Swiper('.swiper-container', {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 });
 
 // Scroll Reveal: Mostrar os elementos quando der o scroll na página
@@ -53,8 +66,41 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .Testimonials,
-  #contact .text, #contact .links`,
+  #contact .text, #contact .links,
+  footer .brand, footer .social`,
   {
     interval: 100,
   }
 );
+
+// Menu ativo conforme a seção visível na página
+const sections = document.querySelectorAll('main section[id]');
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector(`nav ul li a[href*=${sectionId}]`)
+        .classList.add('active');
+    } else {
+      document
+        .querySelector(`nav ul li a[href*=${sectionId}]`)
+        .classList.remove('active');
+    }
+  }
+}
+
+// When Scroll
+
+window.addEventListener('scroll', () => {
+  changeHeaderWhenScroll();
+  backToTop();
+  activateMenuAtCurrentSection();
+});
